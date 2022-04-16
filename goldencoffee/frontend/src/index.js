5ca618e935e7443271abc7f237c9b86743f11e35
godaften <73276996+godaften@users.noreply.github.com>
2022-04-16 19:40:22 +0200

    Localstorage cart, remove item, add to cart on cards homescreen

--- goldencoffee/frontend/src/index.js
+++ goldencoffee/frontend/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+// import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
+
 import { HelmetProvider } from 'react-helmet-async'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
@@ -7,16 +9,26 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { StoreProvider } from './Store'
 
-ReactDOM.render(
-	<React.StrictMode>
-		<StoreProvider>
-			<HelmetProvider>
-				<App />
-			</HelmetProvider>
-		</StoreProvider>
-	</React.StrictMode>,
-	document.getElementById('root')
-)
+// ReactDOM.render(
+// 	<React.StrictMode>
+// 		<StoreProvider>
+// 			<HelmetProvider>
+// 				<App />
+// 			</HelmetProvider>
+// 		</StoreProvider>
+// 	</React.StrictMode>,
+// 	document.getElementById('root')
+// )
+
+// React 18 change
+const root = ReactDOM.createRoot(	document.getElementById('root'))
+root.render(<React.StrictMode>
+	<StoreProvider>
+		<HelmetProvider>
+			<App />
+		</HelmetProvider>
+	</StoreProvider>
+</React.StrictMode>)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
