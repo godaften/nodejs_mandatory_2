6951e0e0969b96f02143c9240c5163ea3e42ea86
godaften <73276996+godaften@users.noreply.github.com>
2022-04-19 14:38:39 +0200

    Nodemailer attempt - commented out in code. Cleaned up some code. Everything works locally

--- goldencoffee/backend/models/productModel.js
+++ goldencoffee/backend/models/productModel.js
@@ -9,9 +9,6 @@ const productSchema = new mongoose.Schema(
     category: { type: String, required: true },
     description: { type: String, required: true },
     price: { type: Number, required: true },
-    countInStock: { type: Number, required: true },
-    rating: { type: Number, required: true },
-    numReviews: { type: Number, required: true },
   },
   {
     timestamps: true
